9c7f366aef44b1b22d7b64acfc83481122c276cc
sd1vaz <caiovazsd1@gmail.com>
2023-04-18 22:15:56 -0300

    add: DataDoSistema

--- Contas/LibClasses/DadosCarteiras.cs
+++ Contas/LibClasses/DadosCarteiras.cs
@@ -3,5 +3,8 @@
     public class DadosCarteiras
     {
         public List<Carteira> ListaDados = new List<Carteira>();
+        public DateTime DataDoSistema { get; set; } = DateTime.Now;
+
+      
     }
 }
